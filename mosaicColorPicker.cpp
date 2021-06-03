05d13aed4d6229380baed18ee7b52ddcb7cc0aeb
UBC Student <srehman1@thetis.students.cs.ubc.ca>
2021-06-03 08:01:39 -0700

    init commit

---
+++ mosaicColorPicker.cpp
@@ -0,0 +1,11 @@
+#include "mosaicColorPicker.h"
+
+MosaicColorPicker::MosaicColorPicker(int mosaicwidth, PNG& inputimage)
+{
+  /* your code here */
+}
+
+HSLAPixel MosaicColorPicker::operator()(point p)
+{
+  /* your code here */
+}
---
+++ softBorderColorPicker.cpp
@@ -0,0 +1,11 @@
+#include "softBorderColorPicker.h"
+
+SoftBorderColorPicker::SoftBorderColorPicker(unsigned int width, PNG& inputimage, double tol)
+{
+    /* your code here */
+}
+
+HSLAPixel SoftBorderColorPicker::operator()(point p)
+{
+    /* your code here */
+}
